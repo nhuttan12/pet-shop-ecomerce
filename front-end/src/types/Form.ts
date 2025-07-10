19f0803379c96e719e05f5a4173345cf8928ebca
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-07-10 22:33:29 +0700

    remove front end

--- front-end/src/types/Form.ts
+++
@@ -1,9 +0,0 @@
-// types/forms.ts
-export interface PersonalInfoFormProps {
-  personalInfo: {
-    name: string;
-    email: string;
-    phone: string;
-  };
-  onPersonalInfoChange: (field: string, value: string) => void;
-}
--- front-end/src/types/Profile.ts
+++
@@ -1,8 +0,0 @@
-export interface UserProfile {
-  firstName: string;
-  lastName: string;
-  email: string;
-  gender: string;
-  phoneNumber: string;
-  birthday: string;
-}
